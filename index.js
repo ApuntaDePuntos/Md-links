80b3c5545a296f7c4c04dd4328ad1f90be50f0c8
Natalia Rodriguez <nataliarodriguez@MacBook-Pro-de-Natalia.local>
2020-10-07 13:15:11 -0500

    array de objetos con descriocion! check!

--- index.js
+++ index.js
@@ -6,70 +6,87 @@ module.exports = () => {
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 const fs = require('fs');
-const path = require('path'); 
+const path = require('path');
 let MarkdownIt = require('markdown-it');
 const fsPromises = require('fs').promises;
 //path.resolve([...paths]);
 
 // Leer los archivos // Retornar una promesas 
-function toReadFile (file_path) {
+
+function toReadFile(file_path) {
   let textosArchivos = new Promise((resolve, reject) => {
-  fs.readFile( file_path, "utf8", (err, data) => {
-    if (err)  reject(err);
-    else { resolve(data) }
-  });
+    fs.readFile(file_path, "utf8", (err, data) => {
+      if (err) reject(err);
+      else { resolve(data) }
+    });
   })
   return textosArchivos
 };
-
-  function extractData (textoenHTML){
+// Estraer los datos al tener el DOM creado JSDOM 
+function extractData(textoenHTML) {
+  // me falta traer path como parametro 
   let dom = new JSDOM(textoenHTML);
   const document = dom.window.document;
   let links = document.querySelectorAll("a");
-  let contenedorInfo= [];
-  links.forEach(objetos=>{ 
-    
-    if (!contenedorInfo.includes(objetos.href)) { contenedorInfo.push(objetos.textContent , objetos.href) }
+  let contenedorInfo = [];
+  links.forEach(objetos => {
+    if (!contenedorInfo.includes(objetos.href)) { let objeto = new Object();
+      objeto.Url = objetos.href;
+      objeto.text = objetos.textContent;
+      // agregar path como propiedad 
+      contenedorInfo.push(objeto) }
     //contenedorInfo += objetos.textContent , objetos.href })
-  console.log(contenedorInfo)
-    }) 
+  })
+  return contenedorInfo 
 }
+// pasar de formato MD a HTML 
 
-let archivos= fs.readdirSync('./')
+function volverHTML(textolargo) {
+  md = new MarkdownIt();
+  let result = md.render(textolargo);
+  return result
+};
+
+let archivos = fs.readdirSync('./')
 console.log(archivos)
 
 // Metodo No bloqueante. 
 // USAR ESTE METODO para funcion 
-fs.readdir( './' , function (error, files) {
-  
-  if (error) {console.log(error) }
-  else { 
-    files.forEach(filePath => { 
-      if (path.extname(filePath) == ".md") 
-          {console.log(filePath); 
-          toReadFile(filePath)
-          .then((textos)=> {
-          const htmlGigante = volverHTML(textos);
-          extractData(htmlGigante)
-          }) 
-        };
+fs.readdir('./', function (error, files) {
 
-  });  
-}
+  if (error) { console.log(error) }
+  else {
+
+    files.forEach(filePath => {
+      if (path.extname(filePath) == ".md") {
+        console.log(filePath);
+        toReadFile(filePath)
+          .then((textos) => {
+            const htmlGigante = volverHTML(textos);
+            const arrayObjetos = extractData(htmlGigante)
+            console.log (arrayObjetos)
+          })
+            .catch(
+            // Registrar la razón del rechazo
+            function(reason) {
+              console.log('Manejar promesa rechazada ('+reason+') aquí.');
+            });
+      };
+    });
+  }
 });
-function volverHTML (textolargo){
-md = new MarkdownIt();
-let result = md.render(textolargo);
-return result
-};
+
 // Crear una funcion que reciba dos parametros:
 // Parametro uno:  string & segundo OPCIONAL : obj booleano validate 
-const mdLinks = ( path , options = 0) => {
+const mdLinks = (path, options = 0) => {
 
 
   // Creacion let alfanumerica, donde se guarda la operacion // accion que se realice 
   //con los parametros. 
   let contenedor = "";
+
+
+
 
   return contenedor
 }
